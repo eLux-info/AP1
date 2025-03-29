1e101862e794e3cfdb27f74080e4331132afda0a
eLux-info <enzoluxinfo@gmail.com>
2025-03-29 19:35:25 +0100

    Suppression du dossier obsolète

--- ArrasGame/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Supprimer toutes les variables de session
-session_destroy(); // Détruire la session
-
-// Rediriger l'utilisateur vers la page de connexion après déconnexion
-header("Location: index.php");
-exit();
-?>
